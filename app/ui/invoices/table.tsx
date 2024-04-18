946cc97170315cd5f9f0533acb3b21f32d8f67ad
vkprogrammer-001 <vipinkumar212003@gmail.com>
2024-04-19 01:47:32 +0530

    added features

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -32,7 +32,7 @@ export default async function InvoicesTable({
                         width={28}
                         height={28}
                         alt={`${invoice.name}'s profile picture`}
-                      />
+                        />
                       <p>{invoice.name}</p>
                     </div>
                     <p className="text-sm text-gray-500">{invoice.email}</p>
